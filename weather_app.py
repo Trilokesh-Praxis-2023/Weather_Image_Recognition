18f99b54db1262dd5c6510926a5a539986d80429
Trilokesh Ranjan Sarkar <146763912+Trilokesh-Praxis-2023@users.noreply.github.com>
2024-05-31 14:34:53 +0530

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -9,7 +9,7 @@ st.markdown(
     """
     <style>
     .main {
-        background-color: #4040FF; /* Darker shade of blue */
+        background-color: #0000FF; /* Darker shade of blue */
         padding: 20px;
         border-radius: 10px;
         color: black; /* Text color */
